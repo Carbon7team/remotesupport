89e84ac7f7d0c987982060f35e8a32e646062318
filippobrugnolaro <filippo.brugnolaro.fb@gmail.com>
2022-05-07 22:24:42 +0200

    CallArea Unit test demonstration

--- remote-support/src/__test__/UI/Body/CallArea.unit.test.js
+++ remote-support/src/__test__/UI/Body/CallArea.unit.test.js
@@ -1,9 +1,38 @@
 import { render } from "@testing-library/react";
+import { create } from "react-test-renderer";
+import Peer from "peerjs";
 import CallArea from "../../../UI/Body/CallArea/CallArea";
 import { useStore } from "../../../Utilities/contextProvider";
 
 jest.mock("../../../Utilities/contextProvider");
 
 describe("CallArea correct render", () => {
-  test("CallArea render", () => {});
+  const vars = jest.fn().mockReturnValue({
+    peerTech: { current: new Peer() },
+    call: { current: null },
+    connectionDataChannel: { current: null },
+  });
+
+  test("CallArea render not in call", () => {
+    useStore.mockReturnValueOnce({
+      stateUIStore: {
+        callAccepted: false,
+        callEnded: true,
+      },
+    });
+
+    let tree = create(<CallArea vars={vars} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test("CallArea render in call", () => {
+    useStore.mockReturnValueOnce({
+      stateUIStore: {
+        streamTech: null,
+      },
+    });
+
+    let tree = create(<CallArea vars={vars} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
