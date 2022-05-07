c2648045ca29752e0a34122925b71e40fcb18ed1
filippobrugnolaro <filippo.brugnolaro.fb@gmail.com>
2022-05-07 17:40:24 +0200

    Testing use context with mocks

---
+++ remote-support/src/__test__/UI/Body/CallArea.unit.test.js
@@ -0,0 +1,9 @@
+import { render } from "@testing-library/react";
+import CallArea from "../../../UI/Body/CallArea/CallArea";
+import { useStore } from "../../../Utilities/contextProvider";
+
+jest.mock("../../../Utilities/contextProvider");
+
+describe("CallArea correct render", () => {
+  test("CallArea render", () => {});
+});
