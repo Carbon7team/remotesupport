06c9715e12cd38130b4d56a6c5d3c331e5252b69
filippobrugnolaro <filippo.brugnolaro.fb@gmail.com>
2022-05-10 10:31:59 +0200

    inizializzazione unit test DataArea

--- remote-support/src/__test__/UI/Body/DataArea.unit.test.js
+++ remote-support/src/__test__/UI/Body/DataArea.unit.test.js
@@ -13,26 +13,47 @@ describe("DataArea correct render", () => {
     connectionDataChannel: { current: null },
   });
 
-    test("DataArea render not in call", () => {
-        useStore.mockReturnValueOnce({
-            stateUIStore: {
-                callAccepted: false,
-                callEnded: true,
-            },
-        });
-        let tree = create(<DataArea vars={vars} />).toJSON();
-        expect(tree).toMatchSnapshot();
+  test("DataArea render not in call", () => {
+    useStore.mockReturnValue({
+      stateUIStore: {
+        callAccepted: false,
+        callEnded: true,
+      },
     });
+    let tree = create(<DataArea vars={vars} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 
-    test("DataArea render in call", () => {
-        useStore.mockReturnValueOnce({
-            stateUIStore: {
-                callAccepted: true,
-                callEnded: false,
-                availabilityTech: true,
-            },
-        });
-        render(<DataArea vars={vars} />); 
-        expect(document.getElementById("data-display")).toBeInTheDocument();
+  test("DataArea render in call", () => {
+    useStore.mockReturnValue({
+      stateUIStore: {
+        callAccepted: true,
+        callEnded: false,
+        availabilityTech: true,
+      },
+      datasetStore: {
+        states: [
+          { code: "S000", name: "stato attivo", active: true },
+          { code: "S001", name: "stato non attivo", active: false },
+        ],
+        alarms: [
+          { code: "A000", name: "NomeA000", severity: "Critical" },
+          { code: "A001", name: "NomeA001", severity: "Warning" },
+        ],
+        measurements: [
+          {
+            code: "M015",
+            name: "NomeM015",
+            value: 30,
+            unitOfMeasure: "Unità di misura",
+          },
+          { code: "M014", name: "NomeM014", value: 20, unitOfMeasure: "UdM" },
+          { code: "M045", name: "NomeM045", value: 20, unitOfMeasure: "UdM" },
+          { code: "M038", name: "NomeM038", value: 20, unitOfMeasure: "UdM" },
+        ],
+      },
     });
+    render(<DataArea vars={vars} />);
+    expect(document.getElementById("data-display")).toBeInTheDocument();
+  });
 });
